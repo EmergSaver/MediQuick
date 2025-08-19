a06f72a00ab8eac5affed29a8246e1311c053bf4
이다민 <min_0625@kakao.com>
2025-08-19 14:46:48 +0900

    [⚙️ FEAT] 하단 navbar에 맞는 페이지 이동 로직 구현

--- android/app/src/main/java/com/emergsaver/mediquick/MainActivity.java
+++ android/app/src/main/java/com/emergsaver/mediquick/MainActivity.java
@@ -1,15 +1,21 @@
 package com.emergsaver.mediquick;
 
 import android.os.Bundle;
-
+import android.view.MenuItem;
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import nav.HospitalFragment;
+import nav.MapFragment;
+import nav.UserFragment;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,45 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+        // Fragment 초기화
+        Fragment hospitalFrag = new HospitalFragment();
+        Fragment mapFrag = new MapFragment();
+        Fragment userFrag = new UserFragment();
+
+        // 첫 번째 화면일 경우 (지도 보기)
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, mapFrag).commit();
+        }
+
+        // 네비게이션 클릭 시
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment selectedFrag = null;
+
+                int id = menuItem.getItemId();
+                if(id == R.id.nav_hospital) {
+                    selectedFrag = hospitalFrag;
+                } else if(id == R.id.nav_map) {
+                    selectedFrag = mapFrag;
+                } else if(id == R.id.nav_user) {
+                    selectedFrag = userFrag;
+                }
+
+                if(selectedFrag != null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, selectedFrag)
+                            .commit();
+                    return true;
+                }
+
+                return false;
+            }
         });
     }
 }
