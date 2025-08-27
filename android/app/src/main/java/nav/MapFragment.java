eed9b3fc14a8fec3f1cd188a063c420d6cc10a2f
이다민 <min_0625@kakao.com>
2025-08-27 13:30:14 +0900

    [♻️ REFACT] 불필요한 구문 주석 제거 및 주석 해제

--- android/app/src/main/java/nav/MapFragment.java
+++ android/app/src/main/java/nav/MapFragment.java
@@ -266,13 +266,12 @@ public class MapFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-//        EditText search = view.findViewById(R.id.search_text);
-//        mapView = view.findViewById(R.id.map_view);
-//        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+        EditText search = view.findViewById(R.id.search_text);
 
-//        // BottomSheet 초기화
-//        ConstraintLayout bottomSheet = view.findViewById(R.id.bottom_sheet);
-//        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        // BottomSheet 초기화
+        ConstraintLayout bottomSheet = view.findViewById(R.id.bottom_sheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+
         // 마커 클릭 시 병원 이름 표시 TextView
         TextView hospitalNameText = view.findViewById(R.id.hospital_name);
 
@@ -306,168 +305,6 @@ public class MapFragment extends Fragment {
             }
         });
 
-
-//        // editText 누르면 대전 성모 병원으로 이동
-//        search.setOnClickListener(v -> {
-//            if (kakaoMap == null) {
-//                Log.d("MAP_DEBUG", "kakaoMap 아직 null");
-//                return;
-//            }
-//
-//            LatLng targetPos = LatLng.from(36.322848, 127.420258);
-//            LabelLayer labelLayer = kakaoMap.getLabelManager().getLayer();
-//
-//            // 마커 스타일 설정
-//            LabelStyle style = LabelStyle.from(R.drawable.red_marker).setAnchorPoint(0.5f, 1.0f);
-//            LabelStyles styles = kakaoMap.getLabelManager().addLabelStyles(LabelStyles.from(style));
-//            Log.d("MAP_DEBUG", "LabelStyles 추가 완료: " + styles);
-//
-//            Label existing = labelLayer.getLabel("searchMarker");
-//            if(existing != null) {
-//                labelLayer.remove(existing);
-//                Log.d("MAP_DEBUG", "기존 마커 제거됨");
-//            }
-//
-//            // 마커 추가
-//            Label newLabel = labelLayer.addLabel(LabelOptions.from("searchMarker", targetPos).setStyles(styles));
-//            Log.d("MAP_DEBUG", "새 마커 추가 완료: " + newLabel + " 위치: " + targetPos);
-//            newLabel.setTag("대전 성모 병원");
-//
-//            // 마커로 카메라 이동
-//            kakaoMap.moveCamera(CameraUpdateFactory.newCenterPosition(targetPos, 18));
-//            Log.d("MAP_DEBUG", "카메라 이동 완료");
-//
-//            fusedLocationProviderClient.removeLocationUpdates(locationCallback);
-//        });
-//
-//        locationCallback = new LocationCallback() {
-//            @Override
-//            public void onLocationResult(@NonNull LocationResult locationResult) {
-//                super.onLocationResult(locationResult);
-//
-//                if(locationResult == null)
-//                    return;
-//
-//                for(Location location : locationResult.getLocations()) {
-//                    double lat = location.getLatitude();
-//                    double lng = location.getLongitude();
-//                    LatLng currentLng = LatLng.from(lat, lng);
-//
-//                    Log.d("CURRENT_LOCATION", "위도 : " + lat + "경도 : " + lng);
-//
-//                    // 위치 Label 업데이트
-//                    if(locationLabel != null) {
-//                        locationLabel.moveTo(currentLng);
-//                    }
-//
-//                    // kakaoMap이 준비되어 있으면 카메라 이동
-//                    if(kakaoMap != null) {
-//                        // 카메라 이동
-//                        kakaoMap.moveCamera(CameraUpdateFactory.newCenterPosition(currentLng));
-//                    }
-//                }
-//
-//            }
-//        };
-//
-//        // 위치 권한 체크 & 요청
-//        if (ActivityCompat.checkSelfPermission(requireContext(),
-//                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            ActivityCompat.requestPermissions(requireActivity(),
-//                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1000);
-//        }
-//
-//        mapView.start(new MapLifeCycleCallback() {
-//            @Override
-//            public void onMapDestroy() {
-//                // API가 정상적으로 종료된 경우
-//                Log.d("API_CONNECT_END", "API 호출이 정상적으로 종료되었습니다.");
-//            }
-//
-//            @Override
-//            public void onMapError(Exception e) {
-//                // 지도 사용 중 에러 발생 시
-//                Log.e("API_CONNECT_ERROR", e.getMessage());
-//            }
-//        }, new KakaoMapReadyCallback() {
-//            @Override
-//            public void onMapReady(@NonNull KakaoMap map) {
-//                // 인증 후 API가 정상적으로 호출된 경우
-//                Log.d("API_CONNECT_SUCCESS", "API가 정상적으로 호출됨");
-//
-//                // 지도 제어
-//                kakaoMap = map;
-//
-//                // 지도 초기 위치 복원
-//                if (savedCameraPos != null) {
-//                    kakaoMap.moveCamera(CameraUpdateFactory.newCameraPosition(savedCameraPos));
-//                }
-//
-//                // 마커 클릭 이벤트 등록
-//                kakaoMap.setOnLabelClickListener(new KakaoMap.OnLabelClickListener() {
-//                    @Override
-//                    public boolean onLabelClicked(KakaoMap kakaoMap, LabelLayer labelLayer, Label label) {
-//                        String hospitalName = (String) label.getTag();
-//                        Log.d("MAP_DEBUG", "마커 클릭됨: " + hospitalName);
-//                        // BottomSheet 열기
-//                        if (hospitalName != null) {
-//                            hospitalNameText.setText(hospitalName);
-//
-//                            // hospital 객체 초기화
-//                            LatLng pos  = label.getPosition();
-//                            hospital = new Hospital(
-//                                    hospitalName, callText.getText().toString(), pos.getLatitude(), pos.getLongitude()
-//                            );
-//
-//                            // BottomSheet 보이게 변경
-//                            bottomSheet.setVisibility(View.VISIBLE);
-//                        }
-//                        return true;
-//                    }
-//                });
-//
-//                // TrackingManager 초기화
-//                trackingManager = kakaoMap.getTrackingManager();
-//
-//                // 권한 체크 후 마지막 위치 가져오기 (초기 지도 위치 설정)
-//                if(ActivityCompat.checkSelfPermission(requireContext(),
-//                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-//                    fusedLocationProviderClient.getLastLocation()
-//                            .addOnSuccessListener(location -> {
-//                                LatLng startPos;
-//                                if(location != null) {
-//                                    startPos = LatLng.from(location.getLatitude(), location.getLongitude());
-//
-//                                    // Label 추가
-//                                    LabelLayer labelLayer = kakaoMap.getLabelManager().getLayer();
-//
-//                                    // 현재 위치 label
-//                                    locationLabel = labelLayer.addLabel(LabelOptions.from(startPos)
-//                                            .setRank(10)
-//                                            .setStyles(LabelStyles.from(
-//                                                    LabelStyle.from(R.drawable.current_location)
-//                                                            .setAnchorPoint(0.5f, 0.5f))));
-//
-//                                    // 방향 Label
-//                                    headingLabel = labelLayer.addLabel(LabelOptions.from(startPos)
-//                                            .setRank(9)
-//                                            .setStyles(LabelStyles.from(
-//                                                    LabelStyle.from(R.drawable.direction_area)
-//                                                            .setAnchorPoint(0.5f, 1.0f))));
-//
-//                                    // headingLabel이 locationLabel과 함께 이동
-//                                    locationLabel.addSharePosition(headingLabel);
-//
-//                                    // 카메라 이동
-//                                    kakaoMap.moveCamera(CameraUpdateFactory.newCenterPosition(startPos));
-//                                }
-//                            });
-//                } else {
-//                    ActivityCompat.requestPermissions(requireActivity(),
-//                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1000);
-//                }
-//            }
-//        });
 
         locationCallback = new LocationCallback() {
             @Override
