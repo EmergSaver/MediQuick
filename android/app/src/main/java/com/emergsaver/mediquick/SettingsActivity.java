ec3772cdf9dd02e3a2a8797dfe428a4f536eb54e
Leeyeachan-dev <leeyc4375@gmail.com>
2025-08-28 14:47:04 +0900

    [⚙️ FEAT] 로그아웃 기능 구현 [ 🎨 DESIGN] 로그아웃 UI 추가 [🔧 LITTLE_FIX] 이전 settings에 오류 수정 [✏️ RENAME]  settings 이름 수정

--- android/app/src/main/java/com/emergsaver/mediquick/SettingsActivity.java
+++ android/app/src/main/java/com/emergsaver/mediquick/SettingsActivity.java
@@ -7,6 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+
+import com.google.android.material.button.MaterialButton;
+import com.emergsaver.mediquick.LogoutActivity;
+//import com.emergsaver.mediquick.DeleteAccountActivity;
+
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -15,7 +21,24 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_settings);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        MaterialButton btnLogout = findViewById(R.id.btnLogout);
+        MaterialButton btnDeleteAccount = findViewById(R.id.btnDeleteAccount);
+
+        if (btnLogout != null) {
+            btnLogout.setOnClickListener(v ->
+                    startActivity(new Intent(this, LogoutActivity.class))
+            );
+        }
+        /*if (btnDeleteAccount != null) {
+            btnDeleteAccount.setOnClickListener(v ->
+                    startActivity(new Intent(this, DeleteAccountActivity.class))
+            );
+        }*/
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_settings), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
