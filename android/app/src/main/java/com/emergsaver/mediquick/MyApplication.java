86df7c38350720aee08033527570a50eee5f1142
Leeyeachan-dev <leeyc4375@gmail.com>
2025-09-04 14:38:14 +0900

    백업: 어제+오늘 작업 포함

--- android/app/src/main/java/com/emergsaver/mediquick/MyApplication.java
+++ android/app/src/main/java/com/emergsaver/mediquick/MyApplication.java
@@ -3,12 +3,21 @@ package com.emergsaver.mediquick;
 import android.app.Application;
 
 import com.kakao.vectormap.KakaoMapSdk;
+import com.kakao.sdk.common.KakaoSdk;
 
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        // 앱 실행 시 SDK 초기화
-        KakaoMapSdk.init(this, BuildConfig.KAKAO_NATIVE_KEY);
+
+        // 카카오맵 SDK 초기화
+        KakaoMapSdk.init(this, BuildConfig.KAKAO_MAP_KEY);
+
+        // 카카오 로그인 SDK 초기화 (네이티브 앱 키 사용)
+        KakaoSdk.init(this, BuildConfig.KAKAO_LOGIN_KEY);
+        // 임시 확인용 (테스트 후 삭제)
+        android.util.Log.d("KAKAO", "mapKey=" + BuildConfig.KAKAO_MAP_KEY);
+        android.util.Log.d("KAKAO", "loginKey=" + BuildConfig.KAKAO_LOGIN_KEY);
+        android.util.Log.d("KAKAO", "keyHash=" + com.kakao.sdk.common.util.Utility.INSTANCE.getKeyHash(this));
     }
 }
