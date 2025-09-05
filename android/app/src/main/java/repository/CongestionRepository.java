824f37ff0635b267602eab6518c6fe1ca225af2b
이다민 <min_0625@kakao.com>
2025-09-05 10:00:36 +0900

    [🐛 BUG_FIXED] 시간 내림차순으로 정렬 안되는 버그 수정 및 로그 출력

--- android/app/src/main/java/repository/CongestionRepository.java
+++ android/app/src/main/java/repository/CongestionRepository.java
@@ -2,11 +2,17 @@ package repository;
 
 import android.util.Log;
 
+import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.protobuf.Value;
 
+import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class CongestionRepository {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -20,17 +26,32 @@ public class CongestionRepository {
     public void fetchLatestAnalysis(OnAnalysisLoaded callback) {
         db.collection("analysis")
                 .orderBy("datetime", Query.Direction.DESCENDING)    // 최신 시간 순 정렬
+                .limit(1)
                 .get()
                 .addOnSuccessListener(queryDocumentSnapshots -> {
                     List<Integer> peopleCounts = new ArrayList<>();
-                    for (var document : queryDocumentSnapshots.getDocuments()) {
+                    if(!queryDocumentSnapshots.isEmpty()) {
+                        var document = queryDocumentSnapshots.getDocuments().get(0);
                         Object countObj = document.get("people_count");
+                        Object time = document.get("datetime");
 
-                        Log.d("ANALYSIS_LOG", "Raw people_count object: " + countObj);
+                        if (time instanceof com.google.firebase.Timestamp) {
+                            com.google.firebase.Timestamp ts = (com.google.firebase.Timestamp) time;
+                            Date date = ts.toDate();
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA);
+                            String formattedTime = sdf.format(date);
 
-                        if (countObj != null) {
+                            Log.d("ANALYSIS_LOG", "Raw people_count object: " + countObj);
+                            Log.d("ANALYSIS_LOG", "Formatted Time: " + formattedTime);
+                        } else {
+                            Log.d("ANALYSIS_LOG", "datetime is not a Timestamp: " + time);
+                        }
+
+                        if(countObj != null) {
                             callback.onLoaded(countObj);
                         }
+                    }else {
+                        Log.d("ANALYSIS_LOG", "No analysis data found");
                     }
                 })
                 .addOnFailureListener(e -> Log.e("ANALYSIS_LOG", "Error fetching analysis", e));
