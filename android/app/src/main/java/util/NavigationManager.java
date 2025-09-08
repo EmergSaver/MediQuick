c09bd79bc3a497b3056c3a1d05d9532e5301bd0f
이다민 <min_0625@kakao.com>
2025-09-08 15:45:03 +0900

    [⚙️ FEAT] 사용자 현재 위치 출발지로부터 해당 병원을 도착지로 네비게이션 기능 구현

---
+++ android/app/src/main/java/util/NavigationManager.java
@@ -0,0 +1,54 @@
+package util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Parcelable;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NavigationManager {
+    // 네비게이션 실행
+    public static void startNavigation(Context context, double startLat, double startLng, double destLat, double destLng, String destName) {
+        List<Intent> intentList = new ArrayList<>();
+
+        // 구글 지도
+        Uri googleIntentUri = Uri.parse("http://maps.google.com/maps?saddr=" + startLat + "," + startLng +
+                "&daddr=" + destLat + "," + destLng + "&dirflg=d");
+        Intent googleMapIntent = new Intent(Intent.ACTION_VIEW, googleIntentUri);
+        intentList.add(googleMapIntent);
+
+        // 카카오 네비
+        String kakaoUrl = "kakaonavi://navigate?ep=" + destLat + "," + destLng +
+                "&by=CAR&ap=" + startLat + "," + startLng +
+                "&name=" + Uri.encode(destName);
+        Intent kakaoIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(kakaoUrl));
+        intentList.add(kakaoIntent);
+
+        // 네이버 지도
+        String naverUrl = "nmap://route/car?slat=" + startLat + "&slng=" + startLng +
+                "&sname=내위치&dlat=" + destLat + "&dlng=" + destLng +
+                "&dname=" + Uri.encode(destName);
+        Intent naverIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(naverUrl));
+        intentList.add(naverIntent);
+
+        //실행 가능한 앱이 없을 때
+        List<Intent> availableIntents = new ArrayList<>();
+        for (Intent intent : intentList) {
+            if (intent.resolveActivity(context.getPackageManager()) != null) {
+                availableIntents.add(intent);
+            }
+        }
+
+        if (availableIntents.isEmpty()) {
+            Toast.makeText(context, "네비게이션 앱이 설치되어 있지 않습니다.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent chooser = Intent.createChooser(intentList.remove(0), "네비게이션 앱 선택");
+        chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentList.toArray(new Parcelable[0]));
+        context.startActivity(chooser);
+    }
+}
