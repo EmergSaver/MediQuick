134a47105789f399478a74dfb1e578148eb50b24
이다민 <min_0625@kakao.com>
2025-09-09 14:22:10 +0900

    [⚙️ FEAT] 마커 상세 페이지에서 길찾기 버튼 기능 추가 및 메서드 분리

--- android/app/src/main/java/util/NavigationUtil.java
+++ android/app/src/main/java/util/NavigationUtil.java
@@ -2,12 +2,14 @@ package util;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 
+import com.emergsaver.mediquick.DetailHospitalActivity;
 import com.google.android.gms.location.FusedLocationProviderClient;
 
 import model.Hospital;
