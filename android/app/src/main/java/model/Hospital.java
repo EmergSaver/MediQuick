992f5f0a15f90ebb25c587bc8116c49f83a778b3
이다민 <min_0625@kakao.com>
2025-09-11 14:30:24 +0900

    [⚙️ FEAT] 혼잡도에 따른 지도 마커 색상 변경

--- android/app/src/main/java/model/Hospital.java
+++ android/app/src/main/java/model/Hospital.java
@@ -1,9 +1,9 @@
 package model;
 
-import java.io.Serializable;
 import java.util.List;
+import java.io.Serializable;
 
-public class Hospital implements Serializable {
+public class Hospital implements Serializable{
     private String id;
     private String hospital_name;
     private String phone;
@@ -11,13 +11,14 @@ public class Hospital implements Serializable {
     private double latitude;
     private double longitude;
     private int doctor_count;
+    private int currentPeople;
     private List<Specialty> specialties;
 
     // 기본 생성자
-    public Hospital() {}
+    public Hospital() {
+    }
 
-    public Hospital(String id, String hospital_name, String phone, String address,
-                    Double latitude, Double longitude, int doctor_count) {
+    public Hospital(String id, String hospital_name, String phone, String address, Double latitude, Double longitude, int doctor_count) {
         this.id = id;
         this.hospital_name = hospital_name;
         this.phone = phone;
@@ -27,28 +28,76 @@ public class Hospital implements Serializable {
         this.doctor_count = doctor_count;
     }
 
-    // Getter / Setter
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
+    // getter 생성
+    public String getId() {
+        return id;
+    }
 
-    public String getHospital_name() { return hospital_name; }
-    public void setHospital_name(String hospital_name) { this.hospital_name = hospital_name; }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
+    public String getHospital_name() {
+        return hospital_name;
+    }
 
-    public String getAddress() { return address; }
-    public void setAddress(String address) { this.address = address; }
+    public void setHospital_name(String hospital_name) {
+        this.hospital_name = hospital_name;
+    }
 
-    public double getLatitude() { return latitude; }
-    public void setLatitude(double latitude) { this.latitude = latitude; }
+    public String getPhone() {
+        return phone;
+    }
 
-    public double getLongitude() { return longitude; }
-    public void setLongitude(double longitude) { this.longitude = longitude; }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
-    public int getDoctor_count() { return doctor_count; }
-    public void setDoctor_count(int doctor_count) { this.doctor_count = doctor_count; }
+    public String getAddress() {
+        return address;
+    }
 
-    public List<Specialty> getSpecialties() { return specialties; }
-    public void setSpecialties(List<Specialty> specialties) { this.specialties = specialties; }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public List<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(List<Specialty> specialties) {
+        this.specialties = specialties;
+    }
+
+    public int getDoctor_count() {
+        return doctor_count;
+    }
+
+    public void setDoctor_count(int doctor_count) {
+        this.doctor_count = doctor_count;
+    }
+
+    public int getCurrentPeople() {
+        return currentPeople;
+    }
+
+    public void setCurrentPeople(int currentPeople) {
+        this.currentPeople = currentPeople;
+    }
 }
